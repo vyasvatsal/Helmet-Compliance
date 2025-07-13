521b4a45516f67953d0df5911ebadb81b6a2f167
vyasvatsal <111115251+vyasvatsal@users.noreply.github.com>
2025-07-13 10:07:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -106,14 +106,13 @@ if start and not st.session_state.violated:
             st.info("ℹ️ No helmet-related objects detected at this threshold.")
 
         for cls_id, conf, (x1, y1, x2, y2) in detections:
-                if  cls_id == 0: NO Helmet
+            if cls_id == 1:  # ON. Helmet
+                label = f"🟢 {LABELS[cls_id]} ({conf:.2f})"
+                color = (0, 255, 0)
+            else:  # NO Helmet
                 label = f"🔴 {LABELS[cls_id]} ({conf:.2f})"
                 color = (0, 0, 255)
                 alert = True
-                else: ON. Helmet
-                label = f"🟢 {LABELS[cls_id]} ({conf:.2f})"
-                color = (0, 255, 0) 
-
 
             cv2.rectangle(frame, (x1, y1), (x2, y2), color, 2)
             cv2.putText(frame, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, color, 2)
